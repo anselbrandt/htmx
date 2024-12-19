4ab7b43db59343e0a885dc8a1d7cedde4e8d4054
anselbrandt <ansel@anselbrandt.com>
2024-12-19 15:34:25 -0800

    Form working

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ uvicorn main:app --reload
 Served at `localhost:8000`
 
 Or reverse proxied to `<hostname>/api`
+
+```
+curl -H 'Content-Type: application/json' \
+      -d '{ "url":"magnet:?xt=urn:btih:CEFA1FF7112FEDE4F96A04DB3B429706D5BD18EC&dn=Kanye+West+-+ye+%282018%29+Mp3+%28320kbps%29+%5BHunter%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Feddie4.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Feddie4.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cypherpunks.ru%3A6969%2Fannounce&tr=udp%3A%2F%2Finferno.demonoid.pw%3A3418%2Fannounce&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fpubt.in%3A2710%2Fannounce&tr=udp%3A%2F%2Fzephir.monocul.us%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.uw0.xyz%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce"}' \
+      -X POST \
+      https://air.anselbrandt.net/api/add
+```
--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from typing import Optional
 from fastapi import FastAPI, Request, Header
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from transmission_rpc import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TRANSMISSION_USERNAME = os.getenv("TRANSMISSION_USERNAME")
+TRANSMISSION_PASSWORD = os.getenv("TRANSMISSION_PASSWORD")
 
 rootPath = "/api"
 
@@ -9,15 +18,28 @@ app = FastAPI(root_path=rootPath)
 
 templates = Jinja2Templates(directory="templates")
 
+c = Client(
+    host="raspberrypi.anselbrandt.net",
+    port=9091,
+    username=TRANSMISSION_USERNAME,
+    password=TRANSMISSION_PASSWORD,
+)
+
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request, hx_request: Optional[str] = Header(None)):
-    films = [
-        {"name": "Blade Runner", "director": "Ridley Scott"},
-        {"name": "Pulp Fiction", "director": "Quentin Tarantino"},
-        {"name": "Mulholland Drive", "director": "David Lynch"},
-    ]
-    context = {"request": request, "films": films, "rootPath": rootPath}
-    if hx_request:
-        return templates.TemplateResponse("partials/table.html", context)
+    context = {"request": request, "rootPath": rootPath}
     return templates.TemplateResponse("index.html", context)
+
+
+class MagnetLink(BaseModel):
+    url: str
+
+
+@app.post("/add")
+async def magnet(magnetlink: MagnetLink):
+    url = magnetlink.url
+    res = c.add_torrent(url)
+    print(res)
+
+    return "success"
